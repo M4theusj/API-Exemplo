69a5d690c3520287fef1bfa537ef8fe9bc656f08
M4theusj <matheusjacob312@hotmail.com>
2023-11-08 15:37:46 -0300

    feat: Exercicios propostos realizados

--- js/script.js
+++ js/script.js
@@ -12,6 +12,29 @@ function getPokemonList(){
     })
 }
 
+// function getPokemonId(){
+//     fetch(BASEURL + 5 ).then( (Response) => { Response.json()}
+// }
+
+function showSixPokemon(){
+    let listaPokemons = []
+    fetch(BASEURL + '?limit=6').then( Response => {
+        if (Response.status == 200){
+            listaPokemons = Response.json().then( json => {json.results.map( pokemon => { 
+                let liPokemon = document.createElement("li")
+                let liPokemonImg = document.createElement("img")
+                fetch(pokemon.url).then(Response => { pokemonImg = Response.json().then( (pokemon) => {(liPokemonImg.src = pokemon.sprites.front_default)})})
+                liPokemon.innerHTML = pokemon.name
+                liPokemon.appendChild(liPokemonImg)
+                document.getElementById("ListaPokemons").appendChild(liPokemon)
+            })
+            return json})
+            return listaPokemons
+        }
+    })
+}
+
+
 function showPokemonList(){
     let listaPokemons = []
     fetch(BASEURL + '?limit=1000').then( Response => {
@@ -39,3 +62,8 @@ async function getData() {
        console.log(pokemon)       
        return(pokemon)
  }*/
+
+ // PROJETOS DE FIXAÇÃO
+ // 1. Criar um site em conjunto com DESIGN 
+ // 2. Criar um projeto Pokedex, criar um site que possa listar 150 pokemons em cards contendo o nome do pokemon, número e o nome.
+ // 3. Quando vc clicar no pokemon, vai aparecer um +, vc clica no + e captura o pokemon, maximo de 6. ( LISTA )
